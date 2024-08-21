688b4f2eb3ce17eb4fce9b33a373d28e25a0e417
Ruvend <ruvi93@gmail.com>
2024-08-21 10:04:03 -0500

    [Gen-AI] CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -3,7 +3,6 @@
             This Calculate Circle Area using Java Example shows how to calculate
             area of circle using it's radius.
     */
-     
     import java.io.BufferedReader;
     import java.io.IOException;
     import java.io.InputStreamReader;
